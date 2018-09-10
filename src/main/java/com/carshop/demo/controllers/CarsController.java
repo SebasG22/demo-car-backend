a9dfadd3897126576d88db31e229db3369828e23
Sebastian Guevara <rlxsebas@gmail.com>
2018-09-10 01:18:55 -0500

    [ CORS ] - habilitando cors

--- src/main/java/com/carshop/demo/controllers/CarsController.java
+++ src/main/java/com/carshop/demo/controllers/CarsController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,6 +21,7 @@ import com.carshop.demo.models.Car;
 import com.carshop.demo.repositories.CarRepository;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/api/v1/cars")
 public class CarsController {
 	
