5ab0357eef4e92f37f402504a1d9f92c0065f326
Sebastian Guevara <rlxsebas@gmail.com>
2018-09-10 01:19:25 -0500

    [ CORS ] - Habilitando metodos HTTP

--- src/main/java/com/carshop/demo/controllers/CarsController.java
+++ src/main/java/com/carshop/demo/controllers/CarsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import com.carshop.demo.models.Car;
 import com.carshop.demo.repositories.CarRepository;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="*", methods = { RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE})
 @RequestMapping("/api/v1/cars")
 public class CarsController {
 	
